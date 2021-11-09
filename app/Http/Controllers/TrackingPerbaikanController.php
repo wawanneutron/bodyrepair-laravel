b0b577a7ec827cc42a5e0c4e20110f0146a86e52
wawanneutron <wawanneutron1331@gmail.com>
2021-11-09 19:55:11 +0700

    alert tracking

--- app/Http/Controllers/TrackingPerbaikanController.php
+++ app/Http/Controllers/TrackingPerbaikanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Booking;
 use Illuminate\Http\Request;
+use stdClass;
 
 class TrackingPerbaikanController extends Controller
 {
--- resources/views/pages/tracking.blade.php
+++ resources/views/pages/tracking.blade.php
@@ -18,14 +18,7 @@
     </div>
     <section class="form-booking">
         <div class="row container-fluid justify-content-center form-pengajuan">
-            <div class="col-md-8">
-                {{-- <div>
-                    @if ($pengerjaan->pengerjaan->status == 'proses')
-                        <div class="alert alert-info mt-5">Kendaraan Anda sedang dalam <strong>pengerjaan</strong> </div>
-                    @elseif($pengerjaan->pengerjaan->status == 'selesai')
-                        <div class="alert alert-success mt-5">Kendaraan Anda sudah <strong>Selesai</strong> dikerjakan, silahkan melakukan pembayaran</div>
-                    @endif
-                </div> --}}
+            <div class="col-md-10">
                 <!-- Page Heading -->
                 {{-- <h1 class="h3 mb-2 text-gray-800">Customer Booking No#<strong>{{ $pengerjaan->booking->no_booking }}</strong> </h1> --}}
                 <div class="card  mb-4">
@@ -161,6 +154,11 @@
 
                             </div>
                             <div class="tab-pane fade" id="info" role="tabpanel" aria-labelledby="info-tab">
+                                @if ($tracking->pengerjaan->status == 'proses')
+                                    <div class="alert alert-info mt-2">Kendaraan Anda sedang dalam <strong>pengerjaan</strong> </div>
+                                @elseif($tracking->pengerjaan->status == 'selesai')
+                                    <div class="alert alert-success mt-2">Kendaraan Anda sudah <strong>Selesai</strong> dikerjakan, silahkan datang ke lokasi untuk pengambilan unit</div>
+                                @endif
                                 <div class="table-responsive">
                                     <table class="table  text-center table-striped" id="dataTable" width="100%" cellspacing="0">
                                         <thead>
