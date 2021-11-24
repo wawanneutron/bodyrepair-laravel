797cc17799b5dc51035680661c77e89b2d5b3571
wawanneutron <wawanneutron1331@gmail.com>
2021-11-24 11:38:59 +0700

    fix bug storage image

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -68,7 +69,7 @@ class User extends Authenticatable
     public function getAvatarUrl()
     {
         if ($this->avatar != null) {
-            return asset(url('storage', $this->avatar));
+            return Storage::url($this->avatar);
         } else {
             return 'https://ui-avatars.com/api/?name=' . str_replace(' ', '+', $this->first_name) .
                 '&background=e29000&color=ffffff&size=100';
